c2ebcaf9d6582f27270aae17bbe5044589577983
skbarber <64565579+skbarber@users.noreply.github.com>
2023-08-25 16:30:21 -0700

    minor changes to python_delgate.py

--- Device/python_delegate.py
+++ Device/python_delegate.py
@@ -3,15 +3,7 @@ import importlib
 from pathlib import Path
 
 
-def python_delegate(script_path, method_name, *args, **kwargs):
-    # Convert the script path to a Path object for easier manipulation
-    script_path = Path(script_path)
-    
-    # Add the script's parent directory to sys.path (PYTHONPATH)
-    sys.path.append(str(script_path.parent))
-
-    # Convert script path to module name
-    module_name = script_path.stem  # This gets the filename without the .py extension
+def python_delegate(module_name, method_name, *args, **kwargs):
     
     # Dynamically import the module
     script = importlib.import_module(module_name)
